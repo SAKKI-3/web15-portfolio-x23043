d3ebf7b5a26dd01cc9eee946e37ff5cb262205ba
SAKKI-3 <yuyu0610yuto@gmail.com>
2024-07-29 01:24:02 +0900

    てst

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
             if (targetElement) {
-                targetElement.scrollIntoView({
+                const headerOffset = 80; // ヘッダーの高さに応じて調整
+                const elementPosition = targetElement.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+                window.scrollTo({
+                    top: offsetPosition,
                     behavior: 'smooth'
                 });
             }
